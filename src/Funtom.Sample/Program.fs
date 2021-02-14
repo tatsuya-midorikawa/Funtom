b506b3cb9b422c430a7795802871decaf3f4fb09
midoliy <midorikawa@tech-leaves.com>
2021-02-14 21:57:36 +0900

    💡Funtom.Threading.Tasks.Taskを新規追加

---
+++ src/Funtom.Sample/Program.fs
@@ -0,0 +1,15 @@
+﻿open System.Net.Http
+open type Funtom.Threading.Tasks.Task
+
+[<EntryPoint>]
+let main argv =
+  
+  async {
+    use client = new HttpClient()
+    let! response = await (client.GetAsync("https://midoliy.com"))
+    return! await (response.Content.ReadAsStringAsync())
+  }
+  |> Async.RunSynchronously
+  |> printfn "%s"
+
+  0 
