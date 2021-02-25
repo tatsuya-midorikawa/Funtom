411be7b6a62eca19c624e6698204fee9c4ce279c
midoliy <midorikawa@tech-leaves.com>
2021-02-26 08:30:28 +0900

    🚧一時コミット

--- src/Funtom/Collections/Generic/Dictionary.fs
+++ src/Funtom/Collections/Generic/Dictionary.fs
@@ -15,33 +15,6 @@ module rec Dictionary =
     new(hashCode', next', key', value') =
       { hashCode = hashCode'; next = next'; key = key'; value = value'; }
 
-  [<Struct;>]
-  type Enumerator<'Key, 'Value> =
-    val mutable private entries : Dictionary.Entry<'Key, 'Value>[]
-    val mutable private dictionary : IDictionary<'Key, 'Value>
-    val mutable private index : int
-    val mutable private version : int
-    val mutable private current : 'Value option
-
-    new (dictionary': IDictionary<'Key, 'Value>, entries: Dictionary.Entry<'Key, 'Value>[], version': int) =
-      { entries = entries; dictionary = dictionary'; index = 0; version = version'; current = Option<'Value>.None }
-
-    member __.dispose() = ()
-    member __.moveNext() =
-      if __.version <> __.dictionary.version then
-        System.InvalidOperationException("") |> raise
-      
-      while __.index < __.dictionary.count do
-        for entry in __.entries do
-          
-      __.index <- __.dictionary.count + 1
-      __.current <- Option<'Value>.None
-      false
-
-  type KeyCollection<'Key, 'Value>(dictionary': IDictionary<'Key, 'Value>) =
-    member public __.getEnumerator() =
-      Enumerator(dictionary')
-    
 
 [<System.Serializable; Sealed;>]
 type Dictionary<'Key, 'Value when 'Key: equality>() =
@@ -55,14 +28,40 @@ type Dictionary<'Key, 'Value when 'Key: equality>() =
   let ComparerName = "Comparer"
 
   [<DefaultValue>] val mutable private backets : int[]
-  [<DefaultValue>] val mutable private entries : Dictionary.Entry<'Key, 'Value>[]
+  [<DefaultValue>] val mutable internal entries : Dictionary.Entry<'Key, 'Value>[]
   #if BIT64
   [<DefaultValue>] val mutable private fastModMultiplier : uint64
   #endif
-  [<DefaultValue>] val mutable private count : int
+  [<DefaultValue>] val mutable internal count : int
   [<DefaultValue>] val mutable private freeList : int
   [<DefaultValue>] val mutable private freeCount : int
-  [<DefaultValue>] val mutable private version : int
+  [<DefaultValue>] val mutable internal version : int
   [<DefaultValue>] val mutable private comaperer : System.Collections.Generic.IEqualityComparer<'Key>
-  [<DefaultValue>] val mutable private keys : Dictionary.KeyCollection<'Key, 'Value>
+  [<DefaultValue>] val mutable private keys : KeyCollection<'Key, 'Value>
   [<DefaultValue>] val mutable private values : ValueCollection
+  
+and KeyCollection<'Key, 'Value when 'Key: equality>(dictionary': Dictionary<'Key, 'Value>) =
+  member public __.getEnumerator() =
+    Enumerator(dictionary')
+
+
+and [<Struct;>] Enumerator<'Key, 'Value when 'Key: equality> =
+  val mutable private dictionary : Dictionary<'Key, 'Value>
+  val mutable private index : int
+  val mutable private version : int
+  val mutable private current : 'Value option
+
+  new (dictionary': Dictionary<'Key, 'Value>) =
+    { dictionary = dictionary'; index = 0; version = dictionary'.version; current = Option<'Value>.None }
+
+  member __.dispose() = ()
+  member __.moveNext() =
+    if __.version <> __.dictionary.version then
+      System.InvalidOperationException("") |> raise
+    
+    while __.index < __.dictionary.count do
+      for entry in __.entries do
+        
+    __.index <- __.dictionary.count + 1
+    __.current <- Option<'Value>.None
+    false
