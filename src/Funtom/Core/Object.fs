e02ec6f3f422730d48f595a425edb7e355be90b2
midoliy <midorikawa@tech-leaves.com>
2021-05-27 10:39:55 +0900

    update

---
+++ src/Funtom/Core/Object.fs
@@ -0,0 +1,37 @@
+﻿namespace Funtom
+
+open System.ComponentModel
+open System.Runtime.CompilerServices
+
+type Object() as this =
+  inherit System.Object()
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.Finalize() = base.Finalize()
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  member _.GetType() = base.GetType()
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  member _.MemberwiseClone() = base.MemberwiseClone()
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.ToString() = base.ToString()
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.Equals(obj: obj) = base.Equals(obj)
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.GetHashCode() = base.GetHashCode()
+  
+  static member equals(a: obj, b: obj) =
+    if a = b then true
+    elif a = null || b = null then false
+    else a.Equals(b)
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  static member Equals(a, b) = Object.equals(a, b)
+
+  static member referenceEquals(a: obj, b: obj) = a = b
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  static member ReferenceEquals(a, b) = Object.referenceEquals(a, b)
+
+  member _.getType() = base.GetType()
+  member _.memberwiseClone() = base.MemberwiseClone()
+  member _.toString() = this.getType().ToString()
+  member _.equals(obj: obj) = RuntimeHelpers.Equals(this, obj)
+  member _.getHashCode() = RuntimeHelpers.GetHashCode(this)
+
---
+++ src/Funtom/Core/Type.fs
@@ -0,0 +1,37 @@
+﻿namespace Funtom
+
+open System.ComponentModel
+open System.Runtime.CompilerServices
+
+type Type(t: System.Type) as __ =
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  member _.GetType() = t.GetType() |> Type
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.ToString() = t.ToString()
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.Equals(obj: obj) = t.Equals(obj)
+  [<EditorBrowsable(EditorBrowsableState.Never)>]
+  override _.GetHashCode() = t.GetHashCode()
+
+  member _.raw with get() = t
+  
+  member _.equals(obj: obj) : bool = t.Equals(obj)
+  member _.equals(type': Type) : bool = t.Equals(type'.raw)
+  member _.equals(type': System.Type) : bool = t.Equals(type')
+  member _.findInterfaces(filter: System.Reflection.TypeFilter, filterCriteria: obj) : Type[] = 
+    t.FindInterfaces(filter, filterCriteria) |> Array.map Type
+  member _.findMembers
+    ( memberType: System.Reflection.MemberTypes, 
+      bindingAttr: System.Reflection.BindingFlags, 
+      filter: System.Reflection.MemberFilter, 
+      filterCriteria: obj ) : System.Reflection.MemberInfo[] = 
+    t.FindMembers(memberType, bindingAttr, filter, filterCriteria)
+  member _.getArrayRank() : int = t.GetArrayRank()
+  member _.getConstructor(types: System.Type[]) : System.Reflection.ConstructorInfo = t.GetConstructor(types)
+  member _.getConstructor(types: Type[]) : System.Reflection.ConstructorInfo = t.GetConstructor(types |> Array.map (fun t -> t.raw))
+
+
+  member _.getType() : Type = t.GetType() |> Type
+  member _.toString() : string = t.ToString()
+  member _.getHashCode() : int = t.GetHashCode()
+
