8dd88a1d7526b4744858aac1aeba60f72a738f57
midoliy <midoliy@outlook.jp>
2023-05-14 00:54:47 +0900

    wip

--- src/Funtom.text/src/Funtom.text.perf/Program.fs
+++ src/Funtom.text/src/Funtom.text.perf/Program.fs
@@ -10,6 +10,7 @@ open System.Linq
 open System.Runtime.Intrinsics
 open System.IO
 open FSharp.NativeInterop
+open System.IO.MemoryMappedFiles
 
 #nowarn "9"
 
@@ -29,19 +30,16 @@ type Benchmark () =
   member __.read() =
     use sr = File.OpenText csv
     let mutable c = ' '
-    while 0 <= sr.Peek() do
-      let t = char (sr.Read())
-      c <- t
+    while 0 <= sr.Read() do ()
     c
 
   [<Benchmark>]
   member __.readline() =
     use sr = File.OpenText csv
-    let mutable cache = 0
+    let mutable s = ""
     while 0 <= sr.Peek() do
-      let a = sr.ReadLine()
-      cache <- a.Length
-    cache
+      s <- sr.ReadLine()
+    s
 
   [<Benchmark>]
   member __.readtoend() =
@@ -50,17 +48,97 @@ type Benchmark () =
     s.Length
 
   [<Benchmark>]
-  member __.fsread() =
+  member __.fs_read() =
+    use fs = File.OpenRead csv
+    let p = NativePtr.stackalloc<byte> 1 |> NativePtr.toVoidPtr
+    let mutable buf = Span<byte>(p, 1)
+    for i in 0L..fs.Length - 1L do
+      fs.Read(buf) |> ignore
+    buf[0]
+
+  [<Benchmark>]
+  member __.fs_read128() =
+    use fs = File.OpenText csv
+    let p = NativePtr.stackalloc<char> 128 |> NativePtr.toVoidPtr
+    let mutable buf = Span<char>(p, 128)
+    while 0 < fs.Read(buf) do
+      ()
+    buf[0]
+
+  [<Benchmark>]
+  member __.fs_read256() =
+    use fs = File.OpenText csv
+    let p = NativePtr.stackalloc<char> 256 |> NativePtr.toVoidPtr
+    let mutable buf = Span<char>(p, 256)
+    while 0 < fs.Read(buf) do
+      ()
+    buf[0]
+
+  [<Benchmark>]
+  member __.fs_readbyte() =
     use fs = File.OpenRead csv
     let mutable c = ' 'B
-    while fs.CanRead do
-      let p = NativePtr.stackalloc<byte> 1 |> NativePtr.toVoidPtr
-      let mutable buf = Span<byte>(p, 1)
-      fs.Read(buf) |> ignore
-      c <- buf[0]
+    for i in 0L..fs.Length - 1L do
+      c <- fs.ReadByte() |> byte
     c
+
+  // [<Benchmark>]
+  // member __.mmf_readbyte() =
+  //   use mmf = MemoryMappedFile.CreateFromFile(csv, FileMode.Open)
+  //   use accessor = mmf.CreateViewStream()
+  //   let mutable c = ' 'B
+  //   for i in 0L..accessor.Length - 1L do
+  //     c <- accessor.ReadByte() |> byte
+  //   c
+
+  [<Benchmark>]
+  member __.mmf_readbyte_128() =
+    use mmf = MemoryMappedFile.CreateFromFile(csv, FileMode.Open)
+    use accessor = mmf.CreateViewStream()
+    let p = NativePtr.stackalloc<byte> 128 |> NativePtr.toVoidPtr
+    let mutable buf = Span<byte>(p, 128)
+    while 0 < accessor.Read(buf) do
+      ()
+    buf[0]
+
+  [<Benchmark>]
+  member __.mmf_readbyte_256() =
+    use mmf = MemoryMappedFile.CreateFromFile(csv, FileMode.Open)
+    use accessor = mmf.CreateViewStream()
+    let p = NativePtr.stackalloc<byte> 256 |> NativePtr.toVoidPtr
+    let mutable buf = Span<byte>(p, 256)
+    while 0 < accessor.Read(buf) do
+      ()
+    buf[0]
 
 [<EntryPoint>]
 let main args =
   BenchmarkRunner.Run<Benchmark>() |> ignore
+  
+  // let csv = "./sample.csv" |> System.IO.Path.GetFullPath
+  // use fs = File.OpenRead csv
+
+  // let p = NativePtr.stackalloc<byte> 1 |> NativePtr.toVoidPtr
+  // let mutable buf = Span<byte>(p, 1)
+  // fs.Read(buf) |> ignore
+  // let c = buf[0]
+
+  // let csv = "./sample.csv" |> System.IO.Path.GetFullPath
+  // use mmf = MemoryMappedFile.CreateFromFile(csv, FileMode.Open)
+  // use accessor = mmf.CreateViewStream()
+  // let b = accessor.ReadByte() |> byte
+
+  // let csv = "./sample.csv" |> System.IO.Path.GetFullPath
+  // use mmf = MemoryMappedFile.CreateFromFile(csv, FileMode.Open)
+  // use accessor = mmf.CreateViewAccessor()
+  // let b = accessor.ReadChar(0)
+
+  // let csv = "./sample.csv" |> System.IO.Path.GetFullPath
+  // let p = NativePtr.stackalloc<char> 10 |> NativePtr.toVoidPtr
+  // let mutable buf = Span<char>(p, 10)
+  // use sr = File.OpenText csv
+  
+  // let x = sr.Read(buf)
+  // let x = sr.Read(buf)
+
   0
