bcd9964d659aec4ef0f100a3b019d511691e8afe
midoliy <midoliy@outlook.jp>
2023-05-21 07:50:42 +0900

    wip

--- src/Funtom.text/src/Funtom.text.console/Program.fs
+++ src/Funtom.text/src/Funtom.text.console/Program.fs
@@ -1,6 +1,8 @@
 ﻿open Funtom.text
 
+//let enc = System.Text.UTF8Encoding(false)
 let enc = System.Text.UTF8Encoding(true)
 let csv = System.IO.Path.GetFullPath "./sample.csv"
-for s in Csv.read (csv, enc) do
+for s in Csv.enumerate (csv, enc) do
   printfn "%s" s
+  printfn "----------"
--- src/Funtom.text/src/Funtom.text/Csv.fs
+++ src/Funtom.text/src/Funtom.text/Csv.fs
@@ -27,38 +27,72 @@ module Csv =
   [<Literal>]
   let Backslash = 0x5Cuy
 
-  let public read (csv: string, enc: Encoding) =
-    //let buf = Array.zeroCreate<byte> 512
-    let cache = ResizeArray<byte>(BufferSize * 2)
-    let buf = Array.zeroCreate<byte> 16
+  let public enumerate (csv: string, enc: Encoding) =
+    let buf = Array.zeroCreate<byte> BufferSize
+    let cache = StringBuilder(BufferSize * 2)
+    let inline get_string(src: array<byte>, start, length) =
+      let view = src.AsSpan().Slice(start, length)
+      let str = cache.Append(enc.GetString(view)).ToString()
+      cache.Clear() |> ignore
+      str
+
     seq {    
       use fs = File.OpenRead csv
+      // BOM がある場合, その Byte 数分読み飛ばす.
+      fs.Seek(enc.Preamble.Length, SeekOrigin.Current) |> ignore
       let mutable length = fs.Read(buf)
-      let mutable i = 0
-
+      let mutable last = 0uy
+      let mutable escaped = false
+      let mutable quoted = false
       while 0 < length do
-        //let view = buf.AsSpan().Slice(0, length)
         let mutable last_idx = 0;
         for i = 0 to (length - 1) do
           match buf[i] with
           | CR ->
-            let view = buf.AsSpan().Slice(last_idx, i - last_idx)
-            yield enc.GetString(view)
-          | LF ->            
-            if 0 < i && buf[i-1] = CR
-              // CRLF の場合, 改行文字として扱わない.
+            if quoted
               then ()
-              // CRLF 以外の場合, 改行文字として処理する.
               else
-                let view = buf.AsSpan().Slice(last_idx, i - last_idx)
-                yield enc.GetString(view)
-            ()
+                let s = get_string(buf, last_idx, i - last_idx)
+                last_idx <- i + 1
+                yield s
+          | LF ->
+            if quoted
+              then ()
+              else
+                if (0 < i && buf[i-1] = CR) || (i = 0 && last = CR)
+                  then
+                    // CRLF の場合, 改行文字として扱わなず, LF 分 last_idx を進める.
+                    last_idx <- i + 1
+                  else
+                    // CRLF 以外の場合, 改行文字として処理する.
+                    let s = get_string(buf, last_idx, i - last_idx)
+                    last_idx <- i + 1
+                    yield s
+          | Backslash ->
+            // flag を反転する.
+            //    \  : true
+            //    \\ : false
+            escaped <- not escaped
+          | DoubleQuotation ->
+            // TODO: "" でエスケープできるパターンを追加する
+            quoted <- not quoted
+            //if escaped
+            //  then quoted <- not quoted
+            //  else ()
+            escaped <- false
           | _ ->
+            escaped <- false
             if i = length - 1
-              then 
+              then
+                // 改行文字で終わっていない場合, StringBuilder でキャッシュを持つ
+                // その際, ここでの yield は避け, 必ず CR or LF のところで yield する
                 let view = buf.AsSpan().Slice(last_idx, length - last_idx)
-                yield enc.GetString(view)
-              else ()
+                cache.Append(enc.GetString(view)) |> ignore
+              else
+                // 中間文字の場合, 特に何もせず読み飛ばす.
+                ()
+          if last = CR then last <- 0uy
+        last <- buf[length - 1]
         length <- fs.Read(buf)
     }
     
