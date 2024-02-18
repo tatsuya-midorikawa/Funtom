3600f03b5db59244cf77b31bf9992d0d8ae8839e
midoliy <midoliy@outlook.jp>
2024-02-18 23:03:41 +0900

    wip

--- src/Funtom.winforms/src/Funtom.app/Program.fs
+++ src/Funtom.winforms/src/Funtom.app/Program.fs
@@ -1,2 +1,8 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Funtom.winforms
+
+form [|
+  size { width= 300; height= 200 }
+  text "Hello F#!!"
+|]
+|> show_dialog
+|> ignore
--- src/Funtom.winforms/src/Funtom.winforms/Form.fs
+++ src/Funtom.winforms/src/Funtom.winforms/Form.fs
@@ -1,5 +1,17 @@
 ﻿namespace Funtom.winforms
 
+[<AutoOpen>]
 module Form =
-  let a = 0
+  let form (properties: Property array) =
+    let apply (f: System.Windows.Forms.Form) p =
+      match p with
+      | Anchor anchor -> f.Anchor <- Anchors.convert anchor
+      | Size size -> f.Size <- Size.convert size
+      | Text text -> f.Text <- text
+    
+    let f = new System.Windows.Forms.Form()
+    properties |> Array.iter (apply f)
+    f
 
+  let show (f: System.Windows.Forms.Form) = f.Show(); f
+  let show_dialog (f: System.Windows.Forms.Form) = f.ShowDialog()
