e23e11104ef4a8394fb83d09f862c82ad1a87774
midoliy <midoliy@outlook.jp>
2024-03-16 20:52:42 +0900

    wip

--- src/Funtom.winforms/src/Funtom.winforms/experiment/Forms.fs
+++ src/Funtom.winforms/src/Funtom.winforms/experiment/Forms.fs
@@ -1,35 +1,54 @@
 ﻿namespace Funtom.winforms.exp
 
 open System.Windows.Forms
+open System.Runtime.CompilerServices
 
-[<System.Flags>]
-type Anchors = none= 0 | top= (1 <<< 0) | bottom= (1 <<< 1) | left= (1 <<< 2) | right= (1 <<< 3)
-module private Anchors = let cast (anchors: Anchors) =  anchors |> (int >> enum<AnchorStyles>)
+module controls =
 
-type Style =
-  | Size of System.Drawing.Size
-  | Location of System.Drawing.Point
+  let inline apply (style: Style) (ctrl: 'T when 'T :> Control) = 
+    match style with
+      | Size s -> ctrl.Size <- s
+      | Location l -> ctrl.Location <- l
+      | Anchor a -> ctrl.Anchor <- Anchors.cast a
+      | AutoSize s -> ctrl.AutoSize <- s
+      | Text t -> ctrl.Text <- t
+      | Name n -> ctrl.Name <- n
+      | Image i -> ctrl.BackgroundImage <- i
+      | _ -> raise (exn $"Not supported styles: {style} ({ctrl})")
 
-[<Struct>]
-type Property = { 
-  styles: Style list
-  controls: Control list
-}
-with member __.empty = { styles = []; controls = []}
 
 module forms =
-  [<Struct; System.Runtime.CompilerServices.IsReadOnly>]
-  type form (self: Form) =
+
+  [<Struct; IsReadOnly;>]
+  type form (self: System.Windows.Forms.Form) =
     interface System.IDisposable with member __.Dispose() = self.Dispose()
 
     new (property: Property) = 
-      let f = new Form()
-      property.styles |> List.iter (function
-        | Size s -> f.Size <- s
-        | Location l -> f.Location <- l
-        )
-      new form (f)
+      let rec apply (styles: Style list) (frm: System.Windows.Forms.Form) =
+        match styles with
+          | [] -> frm
+          | style::rest -> 
+              match style with
+                | Icon ico -> frm.Icon <- ico
+                | _ -> controls.apply style frm
+              apply rest frm
+      let frm =
+        new System.Windows.Forms.Form()
+        |> apply property.styles
+      new form(frm)
+
     new (styles: Style list) = new form { styles= styles; controls= [] }
     new (controls: Control list) = new form { styles= []; controls= controls }
 
+    member __.show () = self.Show()
+    member __.show_dialog () = self.ShowDialog()
+    member __.hide () = self.Hide()
+    member __.close () = self.Close()
+    member __.add (ctrl: Control) = self.Controls.Add(ctrl)
+    member __.add (ctrls: Control list) = self.Controls.AddRange(ctrls |> List.toArray)
+    member __.remove (ctrl: Control) = self.Controls.Remove(ctrl)
+    member __.suspend () = self.SuspendLayout()
+    member __.resume (perform_layout: bool) = self.ResumeLayout(perform_layout)
+    member __.controls with get () = self.Controls
+    member __.children () = self.Controls |> Seq.cast<Control> |> List.ofSeq
 
---
+++ src/Funtom.winforms/src/Funtom.winforms/experiment/Properties.fs
@@ -0,0 +1,35 @@
+﻿namespace Funtom.winforms.exp
+
+open System.Windows.Forms
+
+[<System.Flags>]
+type Anchor = none= 0 | top= (1 <<< 0) | bottom= (1 <<< 1) | left= (1 <<< 2) | right= (1 <<< 3)
+module private Anchors = let cast (anchors: Anchor) =  anchors |> (int >> enum<AnchorStyles>)
+
+type Direction = left_to_right= 0 | top_down= 1 | ritght_to_left= 2 | bottom_up= 3
+module private Direction = let cast (direction: Direction) = direction |> (int >> enum<FlowDirection>)
+
+type Dock = none= 0 | top= 1 | bottom= 2 | left= 3 | right= 4 | fill= 5
+module private Dock = let cast (dock: Dock) = dock |> (int >> enum<DockStyle>)
+
+
+type Style =
+  | Size of System.Drawing.Size
+  | Location of System.Drawing.Point
+  | Anchor of Anchor
+  | Directon of Direction
+  | AutoSize of bool
+  | Text of string
+  | Name of string
+  | Image of System.Drawing.Image
+  | Icon of System.Drawing.Icon
+  | Checked of bool
+  | Index of int
+
+
+[<Struct>]
+type Property = { 
+  styles: Style list
+  controls: Control list
+}
+with member __.empty = { styles = []; controls = []}
