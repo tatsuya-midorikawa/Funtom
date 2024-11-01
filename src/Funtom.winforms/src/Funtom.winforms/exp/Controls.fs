27732f6f2b40db6533a6b131fb3af9bea40b6ea9
midoliy <midoliy@outlook.jp>
2024-11-01 22:13:24 +0900

    wip

--- src/Funtom.winforms/src/Funtom.winforms/exp/Controls.fs
+++ src/Funtom.winforms/src/Funtom.winforms/exp/Controls.fs
@@ -13,10 +13,10 @@ module Control =
     (ctrl: ^T) =
       ctrl.ResumeLayout(performLayout); ctrl
 
-  let inline add<^T when ^T: (member Controls: System.Windows.Forms.Control.ControlCollection)>
+  let inline add<^T when ^T: (member Controls: System.Windows.Forms.Control.ControlCollection with get)>
     (child: System.Windows.Forms.Control)
     (ctrl: ^T) =
-      ctrl.Controls.Add(child); ctrl
+      ctrl.get_Controls().Add(child); ctrl
 
   let inline show<^T when ^T: (member Show: unit -> unit)>
     (ctrl: ^T) =
@@ -71,6 +71,24 @@ module Control =
     (ctrl: ^T) =
       ctrl.set_Checked checked'; ctrl
 
+  let inline items<^T, ^U when ^T: (member get_Items:unit -> ^U when ^U :> System.Collections.IEnumerable)>
+    (ctrl: ^T) =
+      ctrl.get_Items()
+
+  let inline controls<^T, ^U when ^T: (member get_Controls:unit -> ^U when ^U :> System.Windows.Forms.Control.ControlCollection)>
+    (ctrl: ^T) =
+      ctrl.get_Controls()
+
+  let inline mainMenuStrip<^T when ^T: (member set_MainMenuStrip: System.Windows.Forms.MenuStrip -> unit)>
+    (menu: System.Windows.Forms.MenuStrip)
+    (ctrl: ^T) =
+      ctrl.set_MainMenuStrip menu; ctrl
+
+  let inline contextMenuStrip<^T when ^T: (member set_ContextMenuStrip: System.Windows.Forms.ContextMenuStrip -> unit)>
+    (menu: System.Windows.Forms.ContextMenuStrip)
+    (ctrl: ^T) =
+      ctrl.set_ContextMenuStrip menu; ctrl
+
 
 
 [<RequireQualifiedAccess>]
@@ -87,6 +105,9 @@ module Form =
   let inline dock (dock: Dock) (form: System.Windows.Forms.Form) = Control.dock dock form
   let inline location (position: Position) (form: System.Windows.Forms.Form) = form.Location <- (Position.toNative position); form
   let inline text (text: string) (form: System.Windows.Forms.Form) = Control.text text form
+  let inline controls (form: System.Windows.Forms.Form) = Control.controls form
+  let inline mainMenuStrip (menu: System.Windows.Forms.MenuStrip) (form: System.Windows.Forms.Form) = Control.mainMenuStrip menu form
+  let inline contextMenuStrip (menu: System.Windows.Forms.ContextMenuStrip) (form: System.Windows.Forms.Form) = Control.contextMenuStrip menu form
 
 
 [<RequireQualifiedAccess>]
@@ -100,7 +121,10 @@ module FlowLayoutPanel =
   let inline direction (direction: Direction) (panel: System.Windows.Forms.FlowLayoutPanel) = Control.direction direction panel
   let inline location (position: Position) (panel: System.Windows.Forms.FlowLayoutPanel) = Control.location position panel
   let inline text (text: string) (panel: System.Windows.Forms.FlowLayoutPanel) = Control.text text panel
-  
+  let inline flowBreak (break': bool) (panel: System.Windows.Forms.FlowLayoutPanel) = panel.SetFlowBreak(panel.Controls.[panel.Controls.Count - 1], break'); panel
+  let inline controls (panel: System.Windows.Forms.FlowLayoutPanel) = Control.controls panel
+  let inline contextMenuStrip (menu: System.Windows.Forms.ContextMenuStrip) (panel: System.Windows.Forms.FlowLayoutPanel) = Control.contextMenuStrip menu panel
+
 
 [<RequireQualifiedAccess>]
 module Button =
@@ -112,6 +136,8 @@ module Button =
   let inline dock (dock: Dock) (btn: System.Windows.Forms.Button) = Control.dock dock btn
   let inline location (position: Position) (btn: System.Windows.Forms.Button) = Control.location position btn
   let inline text (text: string) (btn: System.Windows.Forms.Button) = Control.text text btn
+  let inline controls (btn: System.Windows.Forms.Button) = Control.controls btn
+  let inline contextMenuStrip (menu: System.Windows.Forms.ContextMenuStrip) (btn: System.Windows.Forms.Button) = Control.contextMenuStrip menu btn
   
 
 [<RequireQualifiedAccess>]
@@ -125,6 +151,35 @@ module CheckBox =
   let inline location (position: Position) (chk: System.Windows.Forms.CheckBox) = Control.location position chk
   let inline text (text: string) (chk: System.Windows.Forms.CheckBox) = Control.text text chk
   let inline check (checked': bool) (chk: System.Windows.Forms.CheckBox) = Control.check checked' chk
+
+[<RequireQualifiedAccess>]
+module ComboBox =
+  let inline suspendLayout (cbo: System.Windows.Forms.ComboBox) = Control.suspendLayout cbo
+  let inline resumeLayout (performLayout: bool) (cbo: System.Windows.Forms.ComboBox) = Control.resumeLayout performLayout cbo
+  let inline add (child: System.Windows.Forms.Control) (cbo: System.Windows.Forms.ComboBox) = Control.add child cbo
+  let inline autoSize (autoSize: bool) (cbo: System.Windows.Forms.ComboBox) = Control.autoSize autoSize cbo
+  let inline anchor (anchors: Anchors) (cbo: System.Windows.Forms.ComboBox) = Control.anchor anchors cbo
+  let inline dock (dock: Dock) (cbo: System.Windows.Forms.ComboBox) = Control.dock dock cbo
+  let inline location (position: Position) (cbo: System.Windows.Forms.ComboBox) = Control.location position cbo
+  let inline text (text: string) (cbo: System.Windows.Forms.ComboBox) = Control.text text cbo
+  let inline items (cbo: System.Windows.Forms.ComboBox) = Control.items cbo
+
+[<RequireQualifiedAccess>]
+module MenuStrip =
+  let inline suspendLayout (menu: System.Windows.Forms.MenuStrip) = Control.suspendLayout menu
+  let inline resumeLayout (performLayout: bool) (menu: System.Windows.Forms.MenuStrip) = Control.resumeLayout performLayout menu
+  let inline autoSize (autoSize: bool) (menu: System.Windows.Forms.MenuStrip) = Control.autoSize autoSize menu
+  let inline anchor (anchors: Anchors) (menu: System.Windows.Forms.MenuStrip) = Control.anchor anchors menu
+  let inline dock (dock: Dock) (menu: System.Windows.Forms.MenuStrip) = Control.dock dock menu
+  let inline location (position: Position) (menu: System.Windows.Forms.MenuStrip) = Control.location position menu
+  let inline text (text: string) (menu: System.Windows.Forms.MenuStrip) = Control.text text menu
+  let inline items (menu: System.Windows.Forms.MenuStrip) = Control.items menu
+
+
+
+
+
+
 
 
 
