18faee6580314cdf0db02a44cc1bededf35c2de9
midoliy <midoliy@outlook.jp>
2024-11-16 08:16:34 +0900

    wip

--- src/Funtom.winforms/src/Funtom.winforms/exp/cexpr.fs
+++ src/Funtom.winforms/src/Funtom.winforms/exp/cexpr.fs
@@ -8,9 +8,15 @@ module Cexpr =
     member __.Yield (x) = x
     member __.For (x, f) = f x
     member __.Zero () = __
-
+    
+    [<CustomOperation>]
+    member __.suspend (_) = ctrl.SuspendLayout(); ctrl
+    [<CustomOperation>]
+    member __.resume (_, ?perform: bool) = ctrl.ResumeLayout(perform |> Option.defaultWith (fun () -> false)); ctrl
     [<CustomOperation>]
     member __.add (_, child: System.Windows.Forms.Control) = ctrl.Controls.Add child; ctrl
+    [<CustomOperation>]
+    member __.add (_, children: System.Windows.Forms.Control[]) = ctrl.Controls.AddRange children; ctrl
     [<CustomOperation>]
     member __.text (_, text: string) = ctrl.Text <- text; ctrl
 
